f6d42d4f23d718718b85e1eb278688e54b0497da
liudanning <g.imldn@gmail.com>
2016-05-18 16:47:22 +0800

    Adding elements(source,category) for RSS 2.0 item

--- test/feed.js
+++ test/feed.js
@@ -5,13 +5,15 @@ describe('RSS 2.0', function () {
 
   it('should return a standard feed', function () {
 
+    var date = new Date();
+
     var feed = new Feed({
       title: 'Feed Title',
       description: 'This is my personnal feed!',
       link: 'http://example.com/',
       image: 'http://example.com/image.png',
       copyright: 'All rights reserved 2013, John Doe',
-      updated: new Date(2013, 06, 14), // optional, default = today
+      updated: date, // optional, default = today
 
       author: {
         name: 'John Doe',
@@ -20,15 +22,36 @@ describe('RSS 2.0', function () {
       }
     });
 
+    feed.addItem({
+      title: 'Item Title',
+      description: 'This is a test item',
+      content: 'This is a test item',
+      image: 'http://example.com/test.png',
+      link: 'http://example.com/test/1',
+      guid: 1,
+      author: [{
+        name: 'Author Name'
+      }],
+      source: {
+        name: 'example.com',
+        url: 'http://example.com/'
+      },
+      category: {
+        name: 'test',
+        domain: 'http://example.com/test'
+      },
+      date: date
+    });
+
     var output = '';
 
     output += '<?xml version="1.0" encoding="utf-8"?>\n';
-    output += '<rss version="2.0">\n';
+    output += '<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/">\n';
     output += '    <channel>\n';
     output += '        <title>Feed Title</title>\n';
     output += '        <description>This is my personnal feed!</description>\n';
     output += '        <link>http://example.com/</link>\n';
-    output += '        <lastBuildDate>Sat, 13 Jul 2013 23:00:00 GMT</lastBuildDate>\n';
+    output += '        <lastBuildDate>' + (date).toUTCString() + '</lastBuildDate>\n';
     output += '        <docs>http://blogs.law.harvard.edu/tech/rss</docs>\n';
     output += '        <image>\n';
     output += '            <title>Feed Title</title>\n';
@@ -37,6 +60,21 @@ describe('RSS 2.0', function () {
     output += '        </image>\n';
     output += '        <copyright>All rights reserved 2013, John Doe</copyright>\n';
     output += '        <generator>Feed for Node.js</generator>\n';
+    output += '        <item>\n';
+    output += '            <title><![CDATA[Item Title]]></title>\n';
+    output += '            <link>http://example.com/test/1</link>\n';
+    output += '            <guid>1</guid>\n';
+    output += '            <pubDate>' + (date).toUTCString() + '</pubDate>\n';
+    output += '            <description><![CDATA[This is a test item]]></description>\n';
+    output += '            <content:encoded><![CDATA[This is a test item]]></content:encoded>\n';
+    output += '            <author>\n';
+    output += '                <name>Author Name</name>\n';
+    output += '            </author>\n';
+    output += '            <enclosure url="http://example.com/test.png">\n';
+    output += '            </enclosure>\n';
+    output += '            <source url="http://example.com/">example.com</source>\n';
+    output += '            <category domain="http://example.com/test">test</category>\n';
+    output += '        </item>\n';
     output += '    </channel>\n';
     output += '</rss>';
 
