c45c1d33f18388d31016ac4a614c726541c34139
Jean-Philippe Monette <contact@jpmonette.net>
2025-05-06 17:42:52 +0100

    release `5.0.0`

--- src/json.ts
+++ src/json.ts
@@ -8,7 +8,7 @@ import { Author, Category, Extension, Item } from "./typings";
 export default (ins: Feed) => {
   const { options, items, extensions } = ins;
 
-  let feed: any = {
+  const feed: any = {
     version: "https://jsonfeed.org/version/1",
     title: options.title,
   };
@@ -47,7 +47,7 @@ export default (ins: Feed) => {
   });
 
   feed.items = items.map((item: Item) => {
-    let feedItem: any = {
+    const feedItem: any = {
       id: item.id,
       // json_feed distinguishes between html and text content
       // but since we only take a single type, we'll assume HTML
